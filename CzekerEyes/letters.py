4f1f85117eae33f13f657dd0d1b1a272d90cc29d
Murtadha Al-Dallal <15mad3@caslab.queensu.ca>
2019-10-27 16:29:05 -0400

    tesseract exe

--- CzekerEyes/letters.py
+++ CzekerEyes/letters.py
@@ -42,7 +42,7 @@ def tesseract_recognition(name, thresh=False, blur=False):
 
     # load the image as a PIL/Pillow image, apply OCR, and then delete
     # the temporary file
-    pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
     text = pytesseract.image_to_string(Image.open(filename), lang="pol",
                                        config="-c tessedit_char_whitelist=AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ --psm 10")
     os.remove(filename)
